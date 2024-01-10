a13174fc2ff7aa0a55604a387e141338500296b7
dino monetto <dinolmonetto@gmail.com>
2024-01-10 08:02:24 -0300

    scd commit

---
+++ script.js
@@ -0,0 +1,65 @@
+// Array para almacenar usuarios y contraseñas
+let usuario = [];
+// Con esto hago que el form Register no se vea hasta que haga clic en crear nuevo usuario
+document.getElementById('showRegisterForm').addEventListener('click', () => {
+    document.querySelector('.login').style.display = 'none';
+    document.querySelector('.register').style.display = 'block';
+});
+// Función inicio de sesión
+document.querySelector(".login").addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    let username = document.querySelector(".username").value;
+    let password = document.querySelector(".password").value;
+    
+    // Limpia el campo 
+    document.querySelector(".username").value = "";
+    document.querySelector(".password").value = "";
+    
+    // Busca el usuario en el array
+    let user = usuario.find(user => user.username === username && user.password === password);
+    
+    if (user) {
+        alert("Inicio de sesión exitoso!");
+        
+        // Redirige a ./pages/cards.html
+        window.location.href = "./pages/cards.html";
+    } else {
+        alert("Usuario o contraseña incorrectas. Intenta de nuevo.");
+    }
+    
+    // Mostrar contenido del array en consola
+    console.log("Contenido:", JSON.stringify(usuario, null, 2));
+});
+
+// Función de registro de nuevos usuarios
+document.querySelector(".register").addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newUser = document.querySelector(".newUser").value;
+    let newPass = document.querySelector(".newPass").value;
+    let repeatPass = document.querySelector(".repeatPass").value;
+
+    // Limpia el campo
+    document.querySelector(".newUser").value = "";
+    document.querySelector(".newPass").value = "";
+    document.querySelector(".repeatPass").value = "";
+
+    // Verifica si el usuario ya existe
+    let existing = usuario.find(user => user.username === newUser);
+
+    // Verificar contraseñas (si son iguales)
+    if (newPass !== repeatPass) {
+        alert("Las contraseñas no coinciden. Intente de nuevo.");
+    } else if (existing) {
+        alert("El usuario ya existe. Pobra con otro.");
+    } else {
+        usuario.push({ username: newUser, password: newPass });
+        alert("Usuario registrado con exito!");
+        document.querySelector('.login').style.display = 'block';  // Mostrar 
+        document.querySelector('.register').style.display = 'none';  // Ocultar
+    }
+    
+    // Mostrar contenido en la consola
+    console.log("Contenido:", JSON.stringify(usuario, null, 2));
+});
+
